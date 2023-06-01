ca6e9e8b47e11adc058c9a3c6cc2c491a7defe8d
monusbence <72020098+monusbence@users.noreply.github.com>
2023-06-01 10:14:11 +0200

    Vége

---
+++ Szamonkeres/Nobel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamonkeres
+{
+    class Nobel
+    {
+        int evszam;
+        string típus, keresztnév, vezetéknév;
+
+        public Nobel(int evszam, string típus, string keresztnév, string vezetéknév)
+        {
+            this.evszam = evszam;
+            this.típus = típus;
+            this.keresztnév = keresztnév;
+            this.vezetéknév = vezetéknév;
+        }
+
+        public int Evszam { get => evszam; }
+        public string Típus { get => típus; }
+        public string Keresztnév { get => keresztnév; }
+        public string Vezetéknév { get => vezetéknév; }
+    }
+}
--- Szamonkeres/Program.cs
+++ Szamonkeres/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Szamonkeres
 {
     class Program
     {
+        //3as 5os feladat nem kell
         static void Main(string[] args)
         {
+            string[] szoveg = File.ReadAllLines("C:\\Users\\tanulo\\Downloads\\nobel.csv");
+            List<Nobel> lista = new List<Nobel>();
+
+            foreach (var elem in szoveg.Skip(1))
+            {
+                string[] adatok = elem.Split(';'); 
+                Nobel uj = new Nobel(Convert.ToInt32(adatok[0]), adatok[1], adatok[2], adatok[3]);
+                lista.Add(uj);
+            }
+
+            Console.WriteLine($"4. feladat: {lista.First(x => x.Evszam == 2017 && x.Típus == "irodalmi")}");
+            Console.WriteLine("6. feladat:");
+            Console.WriteLine($"{lista.Where(x => x.Vezetéknév == "Curie"? Console.Write(x.Vezetéknév + x.Keresztnév))}");
+            
+            
+            
+            Console.WriteLine("7. feladat: ");
+            Console.WriteLine($"{lista.GroupBy(x => x.Típus)}{lista.Count(x => x.Típus == "fizikai")}{lista.Count(x => x.Típus == "kémiai"}{lista.Count(x => x.Típus == "orvosi"});
+
+            
+
+
         }
     }
 }
